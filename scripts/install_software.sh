ed6a87286fd2320eacd2dd95da0d7c50a6754d6c
Daniel Cook <danielecook@gmail.com>
2014-09-06 01:34:57 -0500

    Added install software script.

---
+++ scripts/install_software.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+# This script install the required software!
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew tap homebrew/science
+
+# Sequence Analysis
+brew install --devel samtools
+brew install seqtk
+brew install bwa
+
+# wget
+brew install wget
+
+# Cask
+brew install caskroom/cask/brew-cask
+
+# Atom, Mou
+brew cask install atom --appdir=/Applications
+brew cask install mou --appdir=/Applications
+
+# ql markdown
+brew cask install qlmarkdown
+
+# R + Rstudio
+brew install R
+brew cask install rstudio --appdir=/Applications
