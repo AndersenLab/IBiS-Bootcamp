17e69410faa1a28016a097142a3ed908217ef6bf
Daniel Cook <danielecook@gmail.com>
2014-09-07 08:43:03 -0500

    fixed so IGV works

--- scripts/DL238.Variant.Calling.sh
+++ scripts/DL238.Variant.Calling.sh
@@ -24,7 +24,7 @@ bgzip chrIII.fa
 seqtk comp chrIII.fa.gz 
 
 # Cut out first 2M bp; index.
-samtools faidx chrIII.fa.gz chrIII:1-2000000 | bgzip > chrIII.2M.fa.gz
+samtools faidx chrIII.fa.gz chrIII:1-2000000 |  sed -e 's/:1-2000000//g' | bgzip > chrIII.2M.fa.gz
 bwa index chrIII.2M.fa.gz
 
 # Perform alignment.
@@ -33,8 +33,14 @@ bwa mem -t 2 chrIII.2M.fa.gz DL238.chrIII.fq.gz | samtools view -S -b - > DL238.
 # Sort Reads
 samtools sort DL238.chrIII.2M.bam DL238.chrIII.2M.sorted
 
+# Index
+samtools index DL238.chrIII.2M.sorted.bam 
+
 ## Call Variants
-samtools mpileup -uf chrIII.2M.fa.gz  DL238.chrIII.2M.sorted.bam | bcftools call -mv -Ov > DL238.vcf.gz
+samtools mpileup -uf chrIII.2M.fa.gz  DL238.chrIII.2M.sorted.bam | bcftools call -mv -O v | grep -v '##' > DL238.vcf
+
+## Fix VCF So it can be viewed in IGV.
+cat <(echo "##fileformat=VCFv4.1") <(grep -v '##' DL238.vcf)  > DL238.fixed.vcf
 
 ## View Statistics
 bcftools stats DL238.vcf.gz 
