81c1305cd9847ba5c4604ce4d67857ddb3307001
Daniel Cook <danielecook@gmail.com>
2014-09-07 09:06:03 -0500

    Comment out preperation lines.

--- scripts/DL238.Variant.Calling.sh
+++ scripts/DL238.Variant.Calling.sh
@@ -2,9 +2,9 @@
 
 
 # Used to Subset Original
-samtools merge -f DL238.merged.bam BGI1-RET1-DL238-89d1c-85e53.bam  BGI2-RET1-DL238-8b495-58b92.bam  BGI3-RET1a-DL238-d40d2-9916b.bam
-samtools index DL238.merged.bam
-samtools view -h -b DL238.merged.bam chrIII:1-2000000 | samtools bam2fq - > DL238.chrIII.fq
+#samtools merge -f DL238.merged.bam BGI1-RET1-DL238-89d1c-85e53.bam  BGI2-RET1-DL238-8b495-58b92.bam  BGI3-RET1a-DL238-d40d2-9916b.bam
+#samtools index DL238.merged.bam
+#samtools view -h -b DL238.merged.bam chrIII:1-2000000 | samtools bam2fq - > DL238.chrIII.fq
 
 
 # Requires Homebrew, and samtools (dev)
