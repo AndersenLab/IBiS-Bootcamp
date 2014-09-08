aeccfd0c2b80ef2e97bbb0ed20972e35983c2de9
Erik Andersen <erik.andersen@northwestern.edu>
2014-09-08 13:47:29 -0500

    Added say to end of script

--- scripts/DL238.Variant.Calling.sh
+++ scripts/DL238.Variant.Calling.sh
@@ -28,6 +28,8 @@ samtools mpileup -uf chrIII.2M.fa.gz  DL238.chrIII.2M.sorted.bam | bcftools call
 ## Fix VCF So it can be viewed in IGV.
 cat <(echo "##fileformat=VCFv4.1") <(grep -v '##' DL238.vcf)  > DL238.fixed.vcf
 
+say Congratulations! You have assembled your first genome.
+
 ## View Statistics
 ## Doesn't seem to work. Error is "Could not read the file or the file is not indexed: DL238.vcf.gz"
 # bcftools stats DL238.vcf.gz 
