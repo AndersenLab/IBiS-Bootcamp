70d82e2d5f6ff986eaef04e56b5625a5fe3a28a2
Erik Andersen <erik.andersen@northwestern.edu>
2014-09-08 09:06:29 -0500

    Comment out seqtk
    Why keep it? What is shown?

--- scripts/DL238.Variant.Calling.sh
+++ scripts/DL238.Variant.Calling.sh
@@ -20,9 +20,6 @@ wget http://hgdownload.cse.ucsc.edu/goldenPath/ce10/chromosomes/chrIII.fa.gz
 gzip -d chrIII.fa.gz
 bgzip chrIII.fa
 
-# Generate Stats
-seqtk comp chrIII.fa.gz 
-
 # Cut out first 2M bp; index.
 samtools faidx chrIII.fa.gz chrIII:1-2000000 |  sed -e 's/:1-2000000//g' | bgzip > chrIII.2M.fa.gz
 bwa index chrIII.2M.fa.gz
@@ -45,3 +42,6 @@ cat <(echo "##fileformat=VCFv4.1") <(grep -v '##' DL238.vcf)  > DL238.fixed.vcf
 ## View Statistics
 ## Doesn't seem to work. Error is "Could not read the file or the file is not indexed: DL238.vcf.gz"
 # bcftools stats DL238.vcf.gz 
+
+# Generate Stats
+#seqtk comp chrIII.fa.gz 
